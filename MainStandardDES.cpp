4c32a987fe5267abe4a33d772417de96f657c765
epikapa <frankieyji@gmail.com>
2015-04-19 21:31:30 -0400

    START THIS BITCHES

---
+++ MainStandardDES.cpp
@@ -0,0 +1,33 @@
+/* MainSimplifiedDES.c
+author: Christian A. Rodriguez
+Main file of the class library for the class SimplifiedDES
+*/
+
+#include <iostream>
+#include <string>
+#include <iterator>
+#include "StandardDES.h"
+
+int main() {
+    string message;
+    string key;
+    string ciphertext;
+    
+    message = "1111111100000000111111110000000000000000111111110000000011111111";
+    cout << "Testing Standard DES" << endl;
+    cout << "Enter a 64-bit message string: " << endl << message << endl;
+    //cin >> message;
+    
+    //key = "00000000000000000000000000000000000000000000000000000000";
+    key = "0111111101111111011111110111111101111111011111110111111101111111";
+    //key = "0000000000000000000000000000000000000000000000000000000001100011";
+    //key = "0110001101100011011000110110001101100011011000110110001101100011";
+
+    cout << "Enter a 64-bit key string: " << endl << key << endl;
+    //cin >> key;
+    
+    cout << endl;
+    
+    ciphertext = EncryptDES(message, key);
+    cout << ciphertext << endl;
+}
---
+++ StandardDES.h
@@ -0,0 +1,362 @@
+#ifndef SIMPLIFIEDDES_H_
+#define SIMPLIFIEDDES_H_
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <iterator>
+#include <time.h>
+#include <math.h>
+using namespace std;
+
+//-- Function/Class Declarations
+string EncryptDES(string, string);
+string LeftMessage(string);
+string RightMessage(string);
+string FeistelSystem(string, string&);
+void RightExpansion(string&);
+int RightExpansionBeta(string&);
+void KeyPermutation1(string&);
+void LeftShift(string&);
+void KeyPermutation2(string&);
+string XOR(string, string);
+
+//-- Function/Class Implementations
+string EncryptDES(string message, string key) {
+    // 0. Declare output (local) variable(s)
+    string output;
+    string leftM;
+    string rightM;
+    string tempM;
+    srand(time(NULL));
+    // 0.1 Perform Initial Permutation??? (Note: This actually does not help)
+    //... Skip for right now
+    
+    // 1. Split message string into left and right 32 bit strings
+   
+    // Call a left message function into 32-bit string
+    leftM = LeftMessage(message);
+   
+    // Call a right message function into 32-bit string
+    rightM = RightMessage(message);
+
+    // 2. Perform Feistel System function on rightM, and key
+    string feistelM = FeistelSystem(rightM, key);
+//
+//    // 3. XOR Feistel System string 32 bit output with the current leftM 32 bit input (This now becomes the new left side)
+//    string tempLeft;
+//    string tempRight;
+//
+//    // 4. Turn old rightM into new leftM
+//
+//    // 5. Repeat process 15 more times (16 total) (Note: loop only through steps 1-4)
+//    //      
+//    // 6. Perform Final Permutation??? (Note: This actually does not help)
+//    
+//    // 7. Return ciphertext string
+    return output;
+}
+
+string LeftMessage(string message) {
+    //cout <<"The size of the message string is " << message.size() << " 'bits'?" << endl;
+    string output;
+    
+    // This loop iterates through the message and stores only first half (leftM) into output
+    for (int index = 0 ; index < (message.size() / 2); index++)
+    {
+        output += message[index];
+    }
+    
+//    cout << output << endl;
+    return output;
+}
+
+string RightMessage(string message) {
+    string output;
+    
+    // This loop iterates through the message and stores only second half (rightM) into output
+    for (int index = (message.size() / 2) ; index < message.size(); index++)
+    {
+        output += message[index];
+    }
+    
+//    cout << output << endl;
+    return output;
+}
+
+
+string FeistelSystem(string rightMessage, string& key) {
+    // 0. Declare Feistel temporary (local) variable(s)
+    string output;
+    
+    // 1. Expand rightMessage from 32 bits to 48 bits (E-Box Substitution)
+    //cout << "Expand(rightMessage): " << Expand(rightMessage) << endl;
+    RightExpansion(rightMessage);
+    
+//    cout << "key:   " << key << endl;
+    
+    // 2. Mix/Shift key schedule
+    // 2.1 First original 64-bit key is Permuted once to turn it into a 56-bit key
+    KeyPermutation1(key);
+//    cout << "KeyP1: " << key << endl;
+    // 2.2.1 Split new 56-bit key into C & D each 28-bit
+    string c = LeftMessage(key);
+    string d = RightMessage(key);
+    // 2.2 LeftShift (once or twice)
+    LeftShift(c);
+    LeftShift(d);
+    
+    // 2.3 Then Permute once again with either 1 or 2 shift (depending on ith iteration)
+    string key2 = c + d;
+    KeyPermutation2(key2);
+//    cout << "KeyP2: " << key2 << endl;
+    // 2.1 XOR 48-bit rightMessage with new 48-bit key
+    output = XOR(rightMessage, key2);
+    
+    // 3. Divide new 48 bit string into 8 segments (6 bits each)
+
+    // 3.1 Perform S-Box Substitution with each of the 8 segments to create 8 new segments (4 bits each this time)
+
+    // 4. Append all of the 8 (4 bit) segments together and create a new 32 bit string (result of the Feistel System function which is later XORed with leftMessage)
+
+    // 5. Return new string output
+    return output;
+}
+
+//string Expand(string& right) {
+void RightExpansion(string& right) {
+    string output;
+    
+    output.push_back(right[31]);
+    output.push_back(right[0]);
+    output.push_back(right[1]);
+    output.push_back(right[2]);
+    output.push_back(right[3]);
+    output.push_back(right[4]);
+    output.push_back(right[3]);
+    output.push_back(right[4]);
+    output.push_back(right[5]);
+    output.push_back(right[6]);
+    output.push_back(right[7]);
+    output.push_back(right[8]);
+    output.push_back(right[7]);
+    output.push_back(right[8]);
+    output.push_back(right[9]);
+    output.push_back(right[10]);
+    output.push_back(right[11]);
+    output.push_back(right[12]);
+    output.push_back(right[11]);
+    output.push_back(right[12]);
+    output.push_back(right[13]);
+    output.push_back(right[14]);
+    output.push_back(right[15]);
+    output.push_back(right[16]);
+    output.push_back(right[15]);
+    output.push_back(right[16]);
+    output.push_back(right[17]);
+    output.push_back(right[18]);
+    output.push_back(right[19]);
+    output.push_back(right[20]);
+    output.push_back(right[19]);
+    output.push_back(right[20]);
+    output.push_back(right[21]);
+    output.push_back(right[22]);
+    output.push_back(right[23]);
+    output.push_back(right[24]);
+    output.push_back(right[23]);
+    output.push_back(right[24]);
+    output.push_back(right[25]);
+    output.push_back(right[26]);
+    output.push_back(right[27]);
+    output.push_back(right[28]);
+    output.push_back(right[27]);
+    output.push_back(right[28]);
+    output.push_back(right[29]);
+    output.push_back(right[30]);
+    output.push_back(right[31]);
+    output.push_back(right[0]);
+    
+    right = output;
+//    cout << "32-bit output --> 48-bit output: " << endl << output << endl;
+//    cout << "output size: " << output.size() << endl;
+//    return output;
+}
+
+int RightExpansionBeta(string& right)
+{
+    
+    char salt1 = (rand() % 126) + 33;
+    char salt2 = (rand() % 126) + 33;
+
+    
+}
+
+// KeyPermutation turns 64-bit key input into 56-bit permuted output
+// Should only occur (be called) once throughout the entire DES process
+void KeyPermutation1(string& key) {
+    string output;
+    
+    output.push_back(key[56]);
+    output.push_back(key[48]);
+    output.push_back(key[40]);
+    output.push_back(key[32]);
+    output.push_back(key[24]);
+    output.push_back(key[16]);
+    output.push_back(key[8]);
+    output.push_back(key[0]);
+    output.push_back(key[57]);
+    output.push_back(key[49]);
+    output.push_back(key[41]);
+    output.push_back(key[33]);
+    output.push_back(key[25]);
+    output.push_back(key[17]);
+
+    output.push_back(key[9]);
+    output.push_back(key[1]);
+    output.push_back(key[58]);
+    output.push_back(key[50]);
+    output.push_back(key[42]);
+    output.push_back(key[34]);
+    output.push_back(key[26]);
+    output.push_back(key[18]);
+    output.push_back(key[10]);
+    output.push_back(key[2]);
+    output.push_back(key[59]);
+    output.push_back(key[51]);
+    output.push_back(key[43]);
+    output.push_back(key[35]);
+
+    output.push_back(key[62]);
+    output.push_back(key[54]);
+    output.push_back(key[46]);
+    output.push_back(key[38]);
+    output.push_back(key[30]);
+    output.push_back(key[22]);
+    output.push_back(key[14]);
+    output.push_back(key[6]);
+    output.push_back(key[61]);
+    output.push_back(key[53]);
+    output.push_back(key[45]);
+    output.push_back(key[37]);
+    output.push_back(key[29]);
+    output.push_back(key[21]);
+
+    output.push_back(key[13]);
+    output.push_back(key[5]);
+    output.push_back(key[60]);
+    output.push_back(key[52]);
+    output.push_back(key[44]);
+    output.push_back(key[36]);
+    output.push_back(key[28]);
+    output.push_back(key[20]);
+    output.push_back(key[12]);
+    output.push_back(key[4]);
+    output.push_back(key[27]);
+    output.push_back(key[19]);
+    output.push_back(key[11]);
+    output.push_back(key[3]);
+    
+//    cout << "Permuted Key: " << output << endl;
+//    cout << "output.size(): " << output.size() << endl;
+    key = output;
+}
+
+void LeftShift(string& key) {
+    string output = key;
+    string tempChar;
+    
+//    cout << "Key: " << output << endl;
+//    cout << "key.size(): " << output.size() << endl;
+    tempChar = output.front();
+    output.erase(0,1);
+    output = output + tempChar;
+//    cout << "Key: " << output << endl;
+//    cout << "key.size(): " << output.size() << endl;
+    
+    key = output;
+}
+
+void KeyPermutation2(string& key) {
+    static int round = 0;
+    
+    string output;
+    
+    output.push_back(key[13]);
+    output.push_back(key[16]);
+    output.push_back(key[10]);
+    output.push_back(key[23]);
+    output.push_back(key[0]);
+    output.push_back(key[4]);
+    output.push_back(key[2]);
+    output.push_back(key[27]);
+    output.push_back(key[14]);
+    output.push_back(key[5]);
+    output.push_back(key[20]);
+    output.push_back(key[9]);
+    
+    output.push_back(key[22]);
+    output.push_back(key[18]);
+    output.push_back(key[11]);
+    output.push_back(key[3]);
+    output.push_back(key[25]);
+    output.push_back(key[7]);
+    output.push_back(key[15]);
+    output.push_back(key[6]);
+    output.push_back(key[26]);
+    output.push_back(key[19]);
+    output.push_back(key[12]);
+    output.push_back(key[1]);
+    
+    output.push_back(key[40]);
+    output.push_back(key[51]);
+    output.push_back(key[30]);
+    output.push_back(key[36]);
+    output.push_back(key[46]);
+    output.push_back(key[54]);
+    output.push_back(key[29]);
+    output.push_back(key[39]);
+    output.push_back(key[50]);
+    output.push_back(key[44]);
+    output.push_back(key[32]);
+    output.push_back(key[47]);
+    
+    output.push_back(key[43]);
+    output.push_back(key[48]);
+    output.push_back(key[38]);
+    output.push_back(key[55]);
+    output.push_back(key[33]);
+    output.push_back(key[52]);
+    output.push_back(key[45]);
+    output.push_back(key[41]);
+    output.push_back(key[49]);
+    output.push_back(key[35]);
+    output.push_back(key[28]);
+    output.push_back(key[31]);
+
+    //    cout << "Permuted Key: " << output << endl;
+    //    cout << "output.size(): " << output.size() << endl;
+    key = output;
+}
+
+string XOR(string right, string key) {
+    string output;
+    
+    cout << "right:      " << right << endl;
+    cout << "key:        " << key << endl;
+    
+    for (int i = 0; i < 48; i++) {
+        if ((right[i] == key[i]) || (right[i] == '0' && key[i] == '0')) {
+            output = output + "0";
+        }
+        else {
+            output = output + "1";
+        }
+//        output = output + [(right[i] ^ key[i]);
+    }
+    
+    cout << "xor output: " << output << endl;
+    
+    return output;
+}
+
+#endif
